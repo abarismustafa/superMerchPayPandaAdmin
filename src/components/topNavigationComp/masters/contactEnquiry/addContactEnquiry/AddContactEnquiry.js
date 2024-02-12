8c2f1049de80b5aa407d567a1a86a428d97f7c66
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-02-12 17:44:48 +0530

    today mustafa work

--- src/components/topNavigationComp/masters/contactEnquiry/addContactEnquiry/AddContactEnquiry.js
+++ src/components/topNavigationComp/masters/contactEnquiry/addContactEnquiry/AddContactEnquiry.js
@@ -1,7 +1,59 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CustomInputField from '../../../../../common/CustomInputField';
+import { Formik } from 'formik';
+import CustomTextArea from '../../../../../common/CustomTextArea';
 
 const AddContactEnquiry = () => {
+    const initialValues = {
+        date: '',
+        name: '',
+        email: '',
+        mobileNumber: '',
+        message: '',
+    }
+
+    const validate = (values) => {
+        let errors = {};
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const regexMobileNumber = /^[0-9]{10}$/;
+        const regexPanNumber = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
+        const regexGstNumber =
+            /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9A-Z]{1}$/;
+
+        if (!values.date) {
+            errors.date = "Date is required";
+        }
+        if (!values.name) {
+            errors.name = "Name is required";
+        }
+        if (!values.email) {
+            errors.email = "Email is required";
+        } else if (!regexEmail.test(values.email)) {
+            errors.email = "Invalid Email";
+        }
+
+        if (!values.mobileNumber) {
+            errors.mobileNumber = "Mobile Number is required";
+        } else if (!regexMobileNumber.test(values.mobileNumber)) {
+            errors.mobileNumber = "Invalid Mobile Number";
+        }
+
+        if (!values.message) {
+            errors.message = "Message is required";
+        }
+
+
+        return errors;
+    };
+
+    const submitForm = (values) => {
+        console.log(values);
+    };
+
+    const changeHandle = (selectedData) => {
+        // TODO
+    };
     return (
         <div className="row">
             <div className="col-xl-12">
@@ -11,37 +63,112 @@ const AddContactEnquiry = () => {
                             <div className="tbl-caption tbl-caption-2">
                                 <h4 className="heading mb-0">Edit Customer Enquiry</h4>
                             </div>
-                            <form className="tbl-captionn">
-                                <div className="row">
-                                    <div className="col-xl-6 mb-3">
-                                        <label htmlFor="exampleFormControlInput1" className="form-label"> Date <span className="text-danger">*</span></label>
-                                        <input type="date" className="form-control" id="exampleFormControlInput1" />
-                                    </div>
-                                    <div className="col-xl-6 mb-3">
-                                        <label htmlFor="exampleFormControlInput2" className="form-label">Name<span className="text-danger">*</span></label>
-                                        <input type="text" className="form-control" id="exampleFormControlInput2" placeholder='Name' />
-                                    </div>
-                                    <div className="col-xl-6 mb-3">
-                                        <label htmlFor="exampleFormControlInput3" className="form-label">Email<span className="text-danger">*</span></label>
-                                        <input type="email" className="form-control" id="exampleFormControlInput2" placeholder="email" />
+                            <Formik
+                                initialValues={initialValues}
+                                validate={validate}
+                                onSubmit={submitForm}
 
-                                    </div>
-                                    <div className="col-xl-6 mb-3">
-                                        <label htmlFor="exampleFormControlInput3" className="form-label">Mobile<span className="text-danger">*</span></label>
-                                        <input type="number" className="form-control" id="exampleFormControlInput2" placeholder="Provider" />
+                            >
+                                {(formik) => {
+                                    const {
+                                        values,
+                                        handleChange,
+                                        handleSubmit,
+                                        errors,
+                                        touched,
+                                        handleBlur,
+                                        isValid,
+                                        dirty,
+                                    } = formik;
+                                    return (
+                                        <form className="tbl-captionn">
+                                            <div className="row">
+                                                <div className="col-xl-6 mb-3">
 
-                                    </div>
-                                    <div className="col-xl-6 mb-3">
-                                        <label htmlFor="exampleFormControlInput3" className="form-label">Message<span className="text-danger">*</span></label>
-                                        <textarea className="form-control" defaultValue={"Reason"} />
-                                    </div>
+                                                    <CustomInputField
+                                                        type="date"
+                                                        value={values.date}
+                                                        hasError={errors.date && touched.date}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        errorMsg={errors.date}
+                                                        autoFocus={true}
+                                                        id="date"
+                                                        name="date"
 
-                                </div>
-                                <div>
-                                    <Link to='/admin/contact-enquiry' className="btn btn-danger light ms-1">Cancel</Link>
-                                    <button className="btn btn-primary me-1">Submit</button>
-                                </div>
-                            </form>
+                                                    />
+                                                </div>
+                                                <div className="col-xl-6 mb-3">
+
+                                                    <CustomInputField
+                                                        type="text"
+                                                        value={values.name}
+                                                        hasError={errors.name && touched.name}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        errorMsg={errors.name}
+                                                        autoFocus={true}
+                                                        id="Name"
+                                                        name="name"
+                                                        placeholder="Name"
+
+                                                    />
+                                                </div>
+                                                <div className="col-xl-6 mb-3">
+                                                    <CustomInputField
+                                                        type="email"
+                                                        value={values.email}
+                                                        hasError={errors.email && touched.email}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        errorMsg={errors.email}
+                                                        autoFocus={true}
+                                                        id="email"
+                                                        name="email"
+                                                        placeholder="Email"
+                                                    />
+                                                </div>
+                                                <div className="col-xl-6 mb-3">
+
+                                                    <CustomInputField
+                                                        type="number"
+                                                        value={values.mobileNumber}
+                                                        hasError={errors.mobileNumber && touched.mobileNumber}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        errorMsg={errors.mobileNumber}
+                                                        autoFocus={true}
+                                                        id="mobileNumber"
+                                                        name="mobileNumber"
+                                                        placeholder="Mobile"
+                                                    />
+
+                                                </div>
+                                                <div className="col-xl-6 mb-3">
+
+                                                    <CustomTextArea
+                                                        type="text"
+                                                        value={values.message}
+                                                        hasError={errors.message && touched.message}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        errorMsg={errors.message}
+                                                        autoFocus={true}
+                                                        id="message"
+                                                        name="message"
+                                                        placeholder="Message"
+                                                    />
+                                                </div>
+
+                                            </div>
+                                            <div>
+                                                <Link to='/admin/contact-enquiry' className="btn btn-danger light ms-1">Cancel</Link>
+                                                <button className="btn btn-primary me-1">Submit</button>
+                                            </div>
+                                        </form>
+                                    );
+                                }}
+                            </Formik>
 
                         </div>
                     </div>
