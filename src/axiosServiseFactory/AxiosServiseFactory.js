3cc6e249949ccca67025cb0e7ddcdfed74525274
shahanshah <mohommed.shanshah@upgrad.com>
2024-02-16 15:57:15 +0530

    add checksum

--- src/axiosServiseFactory/AxiosServiseFactory.js
+++ src/axiosServiseFactory/AxiosServiseFactory.js
@@ -1,66 +1,52 @@
 import axios from "axios";
 import { baseUrl } from "../baseUrl";
-import CryptoJS from 'crypto-js';
-// import CryptoJS from 'crypto-js/sha256';
-
-
 import {
-    errorHandlerMiddleWare,
-    requestMiddleWare,
-    responseMiddleWare,
+  errorHandlerMiddleWare,
+  requestMiddleWare,
+  responseMiddleWare,
 } from "../httpInterSeptur/HttpInterSeptur";
-
-// jbuyviycutxydrxyrxyddyxyrdx
-// const CryptoJS = require("crypto")
+import CryptoJS from "crypto-js";
 
 function hashStringWithSecretKey(string, secretKey) {
-    var hash = CryptoJS.HmacMD5(string, secretKey);
-    var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
-    hmac.update(string);
-    var hash = hmac.finalize();
-    return hash;
-
-    // const checksum = CryptoJS(string, secretKey).toString();
-    // return checksum
-    // var ciphertext = CryptoJS.AES.encrypt(string, secretKey).toString();
-    // return ciphertext
+  const hmac = CryptoJS.HmacSHA256(string, secretKey);
+  const hash = hmac.toString(CryptoJS.enc.Base64);
+  return hash;
 }
-
-// Create an instance of Axios
 const axiosInstance = axios.create({
-    baseURL: baseUrl,
-    timeout: 10000, // Set timeout to 10 seconds (adjust as needed)
+  baseURL: baseUrl,
+  timeout: 10000, // Set timeout to 10 seconds (adjust as needed)
 });
 
 // Add request interceptor
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const base64 = btoa(JSON.stringify(config.data))
-        const checkSum = hashStringWithSecretKey(base64, 'jbuyviycutxydrxyrxyddyxyrdx')
-        config.data = { base64, checkSum }
+  (config) => {
+    const base64 = btoa(JSON.stringify(config.data));
+    const checkSum = hashStringWithSecretKey(
+      base64,
+      "jbuyviycutxydrxyrxyddyxyrdx"
+    );
+    config.data = { base64, checkSum };
 
-        // Apply request middleware logic
-        const modifiedConfig = requestMiddleWare(config);
+    const modifiedConfig = requestMiddleWare(config);
 
-        return modifiedConfig;
-    },
-    (error) => {
-        // Handle request errors
-        return Promise.reject(error);
-    }
+    return modifiedConfig;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // Add response interceptor
 axiosInstance.interceptors.response.use(
-    (response) => {
-        // Apply response middleware logic
-        const modifiedResponse = responseMiddleWare(response);
-        return modifiedResponse;
-    },
-    (error) => {
-        // Handle response errors
-        return Promise.reject(error);
-    }
+  (response) => {
+    // Apply response middleware logic
+    const modifiedResponse = responseMiddleWare(response);
+    return modifiedResponse;
+  },
+  (error) => {
+    // Handle response errors
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
